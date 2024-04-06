4430f2780b2620c30eb9ac6348c7659ceeb1c512
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 22:43:02 -0300

    WebDev_JS

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+/* alert("Hello World!!!")
+prompt("Enter your name: ")
+confirm("Do you wanna leave?") */
+
+/* var nome = "Gabriel "
+var idade = 23
+console.log(nome + idade); */
+
+var nome = prompt("digite seu nome: ")
+var idade = prompt("Digite sua idade: ")
+var job = prompt("Digite sua profissão: ")
+// console.log("Olá " + nome + ", seja bem-vindo!")
+console.log(`Ola ${nome}, voce tem ${idade} anos e trabalho como ${job}`)
+alert(`Olá ${nome}, você tem ${idade} anos e trabalha como ${job}`)
